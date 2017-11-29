8fd04ecad8991495d08ca554e3da6d1a08de1a48
Dexter Jung <dropyourcoffee@gmail.com>
2017-11-29 09:09:38 +0900

    no message

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -95,47 +95,6 @@ noble.on('discover', async function(peripheral) {
   console.log("[" + (ScanList.length-1) + "] : " + peripheral.advertisement.localName + " " + peripheral.address + " ("+peripheral.rssi+"dBm)");
   //console.log(peripheral.advertisement);
   //console.log(peripheral.address);
-  /*if(peripheral.advertisement.localName==='Zikto-walk' && peripheral.rssi > -60){
-    process.stdout.write(peripheral.advertisement.localName+'...');
-    console.log(peripheral.rssi);
-
-    peripheral = await asyncBLE.Connect(peripheral);
-
-  	serviceMain = await asyncBLE.DiscoverServices(peripheral);
-  	serviceMain = _.filter(serviceMain,{'uuid':ziktoWalk.Gatt.serviceMain.uuid})[0];
-
-  	//let chars = await asyncBLE.DiscoverCharacteristicsByService(serviceMain);
-  	let chars = await asyncBLE.DiscoverCharacteristicsByPeripheral(peripheral);
-
-  	let rwCharacteristics = _.filter(chars,{"uuid":ziktoWalk.Gatt.serviceMain.readWrite.uuid})[0];
-  	let notiCharacteristics = _.filter(chars,{"uuid":ziktoWalk.Gatt.serviceMain.notify.uuid})[0];
-
-  	const findme = ziktoWalk.Protocol.FindMe;
-  	notiCharacteristics.subscribe(err=>{
-  	  if(err) console.log("Error");
-
-      notiCharacteristics.on("data",(data,isNotification) => {
-        data.forEach((d)=>{
-          process.stdout.write(d.toString(16)+" ");
-        });
-        process.stdout.write("\n");
-      });
-
-
-      rwCharacteristics.write(findme,false,(err)=>{
-        if(err) console.log("Error");
-        console.log("Done");
-
-      });
-    });
-
-    //noble.write(peripheral.uuid, ziktoWalk.Gatt.serviceMain.uuid, ziktoWalk.Gatt.serviceMain.readWrite.uuid,findme,false,(err)=>{
-  	  //if(err) console.log("Error");
-  	  //console.log("Done");
-    //});
-
-
-  }*/
 
 });
 
